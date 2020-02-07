c5c9f394ba7b4d426b63518f96dc9eda5ce49e93
Madeline Jimenez <madelineijimenez@gmail.com>
2020-02-07 09:57:29 -0500

    Inner join db tables

--- employee_tracker.js
+++ employee_tracker.js
@@ -1,23 +1,76 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const consoleTable = require("")
+const cTable = require("console.table");
 
+
+// MySQL DB Connection Information
 const connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Andy&Maddy082716",
+    database: "employeeTracker_DB"
+  });
 
-  // Heroku port 8080
-  port: process.env.PORT || 8080,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "Andy&Maddy082716",
-  database: "employeeTracker_DB"
-});
-
+// Checks if connection was successful. If so, prompts are run.
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     runSearch();
   });
+
+function runSearch() {
+    inquirer
+    .prompt({
+    name: "action",
+    type: "rawlist",
+    message: "What would you like to do?",
+    choices: [
+        "View All Employees",
+        "View All Employees By Department",
+        "View All Employees By Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+        "View All Roles"
+    ]
+    })
+    .then(function(answer) {
+    switch (answer.action) {
+    case "View All Employees":
+        allEmployeeSearch();
+        break;
+
+    case "View All Employees By Department":
+        allEmployeesDepart();
+        break;
+
+    case "View All Employees By Manager":
+        allEmployeesManager();
+        break;
+
+    case "Add Employee":
+        addEmployee();
+        break;
+
+    case "Remove Employee":
+        removeEmployee();
+        break;
+    
+    case "Update Employee Role":
+        updateRole();
+        break;
+
+    case "Update Employee Manager":
+        updateManager();
+        break;
+
+    case "View All Roles":
+        allRoleSearch();
+        break;
+    }
+    });
+}
+
+
--- seed.sql
+++ seed.sql
@@ -9,24 +9,39 @@ CREATE TABLE department (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE role {
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
-    salary DECIMAL(5,2),
+    salary INTEGER(6),
     department_id INTEGER(11),
     PRIMARY KEY (id)   
-}
+);
 
-CREATE TABLE employee {
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER(11),
     manager_id INTEGER(11),
     PRIMARY KEY (id)   
-}
+);
+
+
+-- -- Test adding rows
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id)
+-- VALUES ("Madeline", "Jimenez", 75, 10);
+
+-- -- Test deleting rows
+-- DELETE FROM employee WHERE first_name = "Madeline";
 
 
 SELECT * FROM department;
 SELECT * FROM role;
 SELECT * FROM employee;
+
+
+--- Testing inner join to put id, first_name, last_name, title, department, salary and manager into one table
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, employee.manager_id
+FROM ((department
+INNER JOIN role ON department.id = role.id)
+INNER JOIN employee ON department.id = employee.id);
